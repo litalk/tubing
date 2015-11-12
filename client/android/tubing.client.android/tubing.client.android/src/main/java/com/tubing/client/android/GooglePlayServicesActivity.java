874071b35c692f0a91d2f5ff4ab13012ba538cad
kornfeld <lital.kornfeld@hp.com>
2015-11-12 09:23:09 +0200

    get logged in account from google play services

--- client/android/tubing.client.android/tubing.client.android/src/main/java/com/tubing/client/android/GooglePlayServicesActivity.java
+++ client/android/tubing.client.android/tubing.client.android/src/main/java/com/tubing/client/android/GooglePlayServicesActivity.java
@@ -1,11 +1,20 @@
 package com.tubing.client.android;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -31,6 +40,17 @@ public class GooglePlayServicesActivity extends Activity implements
      * waiting for resolution intent to return.
      */
     private boolean mIsInResolution;
+
+    private static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+    private static final int REQUEST_CODE_RECOVER_FROM_AUTH_ERROR = 1001;
+    private static final int REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 1002;
+
+    private String mEmail; // Received from newChooseAccountIntent(); passed to getToken()
+    private String SCOPE = "oauth2:https://www.googleapis.com/auth/userinfo.profile";
+    public static String TYPE_KEY = "type_key";
+    public static final String EXTRA_ACCOUNTNAME = "extra_accountname";
+
+    public enum Type {FOREGROUND, BACKGROUND, BACKGROUND_WITH_SYNC}
 
     /**
      * Called when the activity is starting. Restores the activity state.
@@ -91,12 +111,130 @@ public class GooglePlayServicesActivity extends Activity implements
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQUEST_CODE_RESOLUTION:
-                retryConnecting();
-                break;
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            if (resultCode == RESULT_OK) {
+                mEmail = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                getUsername();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "You must pick an account", Toast.LENGTH_SHORT).show();
+            }
+        } else if ((requestCode == REQUEST_CODE_RECOVER_FROM_AUTH_ERROR ||
+                requestCode == REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR)
+                && resultCode == RESULT_OK) {
+            handleAuthorizeResult(resultCode, data);
+            return;
         }
+        getUsername();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleAuthorizeResult(int resultCode, Intent data) {
+        if (data == null) {
+            show("Unknown error, click the button again");
+            return;
+        }
+        if (resultCode == RESULT_OK) {
+            getTask(this, mEmail, SCOPE).execute();
+            return;
+        }
+        if (resultCode == RESULT_CANCELED) {
+            show("User rejected authorization.");
+            return;
+        }
+        show("Unknown error");
+    }
+
+    /**
+     * Note: This approach is for demo purposes only. Clients would normally not get tokens in the
+     * background from a Foreground activity.
+     */
+    private AbstractGetNameTask getTask(
+            GooglePlayServicesActivity activity, String email, String scope) {
+
+        return new GetNameInBackground(activity, email, scope);
+    }
+
+    /**
+     * This method is a hook for background threads and async tasks that need to update the UI.
+     * It does this by launching a runnable under the UI thread.
+     */
+    public void show(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(GooglePlayServicesActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * This method is a hook for background threads and async tasks that need to provide the
+     * user a response UI when an exception occurs.
+     */
+    public void handleException(final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (e instanceof GooglePlayServicesAvailabilityException) {
+                    // The Google Play services APK is old, disabled, or not present.
+                    // Show a dialog created by Google Play services that allows
+                    // the user to update the APK
+                    int statusCode = ((GooglePlayServicesAvailabilityException) e)
+                            .getConnectionStatusCode();
+                    Dialog dialog = GooglePlayServicesUtil.getErrorDialog(statusCode,
+                            GooglePlayServicesActivity.this,
+                            REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                    dialog.show();
+                } else if (e instanceof UserRecoverableAuthException) {
+                    // Unable to authenticate, such as when the user has not yet granted
+                    // the app access to the account, but the user can fix this.
+                    // Forward the user to an activity in Google Play services.
+                    Intent intent = ((UserRecoverableAuthException) e).getIntent();
+                    startActivityForResult(intent,
+                            REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                }
+            }
+        });
+    }
+
+    /**
+     * Attempts to retrieve the username.
+     * If the account is not yet known, invoke the picker. Once the account is known,
+     * start an instance of the AsyncTask to get the auth token and do work with it.
+     */
+    private void getUsername() {
+        if (mEmail == null) {
+            pickUserAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new GetNameInBackground(GooglePlayServicesActivity.this, mEmail, SCOPE).execute();
+            } else {
+                Toast.makeText(this, "not_online", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * Checks whether the device currently has a network connection
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Starts an activity in Google Play Services so the user can pick an account
+     */
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
     }
 
     private void retryConnecting() {
