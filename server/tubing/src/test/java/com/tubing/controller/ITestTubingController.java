0d12f4e27e91b894a55dd1afd6e8372c7dd71912
Effi <effib@hp.com>
2015-12-25 22:20:09 +0200

    fix encoding test login using integration test

---
+++ server/tubing/src/test/java/com/tubing/controller/ITestTubingController.java
@@ -0,0 +1,66 @@
+package com.tubing.controller;
+
+import com.tubing.common.ObjectMapperUtils;
+import com.tubing.dal.model.Account;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@Ignore
+@RunWith(MockitoJUnitRunner.class)
+public class ITestTubingController {
+
+    private MockMvc _mockMvc;
+
+    @Before
+    public void setUp() {
+
+        _mockMvc = MockMvcBuilders.standaloneSetup(new TubingController()).build();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+
+        _mockMvc.perform(MockMvcRequestBuilders.
+                post("/tubing/login", 1L).
+                contentType(MediaType.APPLICATION_JSON).
+                content(getUserJson())).
+        andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void testAddToPlaylistFormat1() throws Exception {
+
+        String shazam = "Found Travesuras (Remix) by Nicky Jam with Shazam, have a listen: http://shz.am/t146768962";
+        addToPlayList(shazam);
+    }
+
+    @Test
+    public void testAddToPlaylistFormat2() throws Exception {
+
+        String shazam = "I just used Shazam to discover Llévame Contigo by Romeo Santos. http://shz.am/t54018231";
+        addToPlayList(shazam);
+    }
+
+    private void addToPlayList(String shazam) throws Exception {
+
+        _mockMvc.perform(MockMvcRequestBuilders.
+                post("/tubing/playlist", 1L).
+                contentType(MediaType.APPLICATION_JSON).
+                content(shazam)).
+                andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    public String getUserJson() {
+
+        return ObjectMapperUtils.from(
+                new Account("grover@gmail.com", "Krovi Somsom", "470859838725a3b"));
+    }
+}
--- server/tubing/src/test/java/com/tubing/dal/itest/ITestElasticService.java
+++ server/tubing/src/test/java/com/tubing/dal/itest/ITestElasticService.java
@@ -1,12 +1,12 @@
 package com.tubing.dal.itest;
 
+import com.tubing.logic.AccountLogic;
 import com.tubing.dal.ElasticService;
 import com.tubing.dal.model.Account;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.testng.annotations.Test;
 
-import java.net.URLEncoder;
 import java.util.UUID;
 
 @Ignore  // Integration Tests - Elasticsearch needed
@@ -18,7 +18,7 @@ public class ITestElasticService {
         Account krovi = new Account(String.format("krovi_%s@gmail.com", UUID.randomUUID()), "Krovi", "1a");
         ElasticService.insert(krovi);
         Thread.currentThread().sleep(1000);  // let elasticsearch update index with new account
-        Account search = ElasticService.search(Account.class, Account.TYPE, URLEncoder.encode(String.format("%s:\"%s\"", Account.EMAIL_FIELD, krovi.getEmail()), "UTF-8"));
+        Account search = ElasticService.search(Account.class, Account.TYPE, AccountLogic.getSearchByEmailQuery(krovi));
         Assert.assertEquals(krovi.getEmail(), search.getEmail());
     }
 }
