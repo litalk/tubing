9a90f329fab81d4832edc01aa5603ac4bdb0ec96
Effi <effib@hp.com>
2016-01-03 18:55:19 +0200

    youtube scope added

--- server/tubing/src/main/java/com/tubing/logic/google/YouTubeBuilder.java
+++ server/tubing/src/main/java/com/tubing/logic/google/YouTubeBuilder.java
@@ -1,47 +1,45 @@
 package com.tubing.logic.google;
 
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 
 public class YouTubeBuilder {
 
-    private static final JsonFactory _jsonFactory = JacksonFactory.getDefaultInstance();
-    private static HttpTransport _transport;
+//    private static HttpTransport _transport;
 
     public static YouTube build(String accessToken) {
 
+        GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
+
         return new YouTube.Builder(
-                getTransport(),
-                _jsonFactory,
-                getCredential(accessToken)).
+                    new NetHttpTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    credential).
                 setApplicationName("Tubing").
                 build();
     }
 
-    public static Credential getCredential(String accessToken) {
+//    public static Credential getCredential(String accessToken) {
+//
+//        try {
+//            return new Credential(BearerToken.authorizationHeaderAccessMethod()).setAccessToken(accessToken);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-        try {
-            return new Credential(BearerToken.authorizationHeaderAccessMethod()).setAccessToken(accessToken);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public synchronized static HttpTransport getTransport() {
-
-        if(_transport == null) {
-            try {
-                _transport = GoogleNetHttpTransport.newTrustedTransport();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return _transport;
-    }
+//    public synchronized static HttpTransport getTransport() {
+//
+//        if(_transport == null) {
+//            try {
+//                _transport = GoogleNetHttpTransport.newTrustedTransport();
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        return _transport;
+//    }
 }
