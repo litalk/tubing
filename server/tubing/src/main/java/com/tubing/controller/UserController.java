8348cefa2f1a1832aa3845c47d411d51a756646c
kornfeld <lital.kornfeld@hpe.com>
2016-05-01 09:46:58 +0300

    removed redundant exception catching

--- server/tubing/src/main/java/com/tubing/controller/UserController.java
+++ server/tubing/src/main/java/com/tubing/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.tubing.controller;
 
-import java.util.UUID;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.tubing.common.StringUtils;
+import com.tubing.dal.EntityPersister;
+import com.tubing.dal.model.Account;
+import com.tubing.dal.model.Session;
+import com.tubing.logic.UserLogic;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,41 +12,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tubing.common.StringUtils;
-import com.tubing.dal.EntityPersister;
-import com.tubing.dal.model.Account;
-import com.tubing.dal.model.Session;
-import com.tubing.logic.UserLogic;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tubing")
 public class UserController {
-    
+
     @Autowired
     private UserLogic _logic;
     @Autowired
     private EntityPersister _persister;
-    
+
     @RequestMapping(value = "login", method = RequestMethod.POST, headers = "Accept=application/json")
     public LoginResponse login(@RequestBody final String authCode) throws ServletException {
-        
+
         Account account = null;
-        try {
-            account = _logic.login(getAuthCode(authCode));
-        } catch (Exception ex) {
-            account = null;
-        }
-        if (account == null) {
-            throw new ServletException("Invalid login");
-        }
+        account = _logic.login(getAuthCode(authCode));
         String sessionKey = UUID.randomUUID().toString();
         _persister.insert(new Session(sessionKey, account.getUserId()));
-        
+
         return new LoginResponse(JwtHelper.build(account.getName(), sessionKey));
     }
-    
+
     private static String getAuthCode(@RequestBody String json) throws ServletException {
-        
+
         String ret;
         try {
             ret = new JSONObject(json).getString("auth_code");
@@ -55,14 +46,14 @@ public class UserController {
             throw new ServletException(
                     "Invalid body, should look like {auth_code: <authorization code>}");
         }
-        
+
         return ret;
     }
-    
+
     @RequestMapping(value = "logout", method = RequestMethod.POST)
     public void logout(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
-            
+
         String session = null;
         try {
             session = JwtHelper.getSession(JwtHelper.getToken(request));
@@ -78,14 +69,14 @@ public class UserController {
             }
         }
     }
-    
+
     @SuppressWarnings("unused")
     private static class LoginResponse {
-        
+
         public String token;
-        
+
         public LoginResponse(final String token) {
-            
+
             this.token = token;
         }
     }
